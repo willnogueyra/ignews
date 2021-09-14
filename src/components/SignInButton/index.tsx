59e6e8fcffe0ebd3458ab9e6fa27f557fd396fe1
William Nogueira <will.ribeiro@live.com>
2021-09-14 12:40:55 -0300

    Back-end no front-end - API Routes no Next.js - Autenticação com Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,30 @@
 import { FaGithub } from "react-icons/fa"
 import {FiX} from "react-icons/fi"
+import { signIn, signOut, useSession } from "next-auth/client"
 import styles from './styles.module.scss'
 
+
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
+  
+  console.log(session)
 
   return (
-    isUserLoggedIn ? (
+    session ? (
       <button
         type="button"
         className={styles.signInButton}
+        onClick={() => signOut()}
       >
         <FaGithub color="#04d361" />
-        William Nogueira
+        {session.user.name}
         <FiX color="#737380" className={styles.closeIcon}/>
       </button>
     ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
