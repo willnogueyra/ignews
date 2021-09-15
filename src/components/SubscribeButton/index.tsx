fc52ce2547c4a1292173157c2d8f57c119d41ce6
William Nogueira <will.ribeiro@live.com>
2021-09-15 18:34:11 -0300

    Back-end no front-end - Pagamentos no stripe - Gerando sessão de checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from "next-auth/client";
 import styles from "./styles.module.scss";
 
 interface SubscribeProps {
@@ -5,10 +6,22 @@ interface SubscribeProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) { // se não tiver logado, direciona para autenticação do github
+      signIn('github');
+      return;
+    }
+
+    
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
