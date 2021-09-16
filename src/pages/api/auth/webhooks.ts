abbf16d9f1b46f909801b0ca4e557ae6131319bc
William Nogueira <will.ribeiro@live.com>
2021-09-16 20:49:04 -0300

    Back-end no front-end - Ouvindo Webhooks - Ouvindo eventos do Stripe

--- src/pages/api/auth/webhooks.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('evento recebido')
-
-  res.status(200).json({ ok: true})
-}
