fa41ed528ae0d639d68c031f9f0dc06ff5d80dc5
William Nogueira <will.ribeiro@live.com>
2021-09-17 12:52:00 -0300

    Back-end no front-end - Ouvindo Webhooks - Ouvindo mais eventos

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,17 +1,19 @@
 import { fauna } from "../../../services/fauna";
-import { query as q } from "faunadb"
+import { query as q } from 'faunadb'
 import { stripe } from "../../../services/stripe";
 
-export async function saveSubscription (subscriptionId: string, customerId: string) {
-  // buiscar o usuario no banco fauna db com Id {customerId}
-  // salvar os dados da subscription no fauna db
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+  createAction = false
+) {
   const userRef = await fauna.query(
     q.Select(
       "ref",
       q.Get(
         q.Match(
           q.Index('user_by_stripe_customer_id'),
-          customerId,
+          customerId
         )
       )
     )
@@ -26,10 +28,27 @@ export async function saveSubscription (subscriptionId: string, customerId: stri
     price_id: subscription.items.data[0].price.id,
   }
 
-  await fauna.query(
-    q.Create(
-      q.Collection('subscriptions'),
-      { data: subscriptionData }
+  if(createAction) {
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        { data: subscriptionData }
+      )
     )
-  )
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref", 
+          q.Get(
+            q.Match(
+              q.Index('subscription_by_id'),
+              subscriptionId
+            )
+          )
+        ),
+        { data: subscriptionData }
+      ),
+    )
+  }
 }
