bdc9889a3ff9b427bf3c5edcf9a5bdae80165187
William Nogueira <will.ribeiro@live.com>
2021-09-17 18:06:40 -0300

    Front-end JAMStack - Trabalhando com CMS - Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import Head from "next/head";
 import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss"
 import Prismic from "@prismicio/client"
+import { RichText } from "prismic-dom"
 
-export default function Posts() {
+interface Post {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,11 +25,14 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de msdad</time>
-            <strong> Titulo do post</strong>
-            <p> texto do post</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
+          
           <a href="">
             <time>12 de msdad</time>
             <strong> Titulo do post</strong>
@@ -47,9 +62,21 @@ export default function Posts() {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '', // encontrar
+      // conteudo que o tipo é paragrafo se for texto, se nao, nulo
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {posts}
   }
  }
