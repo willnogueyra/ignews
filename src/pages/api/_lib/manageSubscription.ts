aed68fcb31cae508fddbd99ae91f38ac8c8d4d1c
William Nogueira <will.ribeiro@live.com>
2021-09-17 10:24:49 -0300

    Back-end no front-end - Ouvindo Webhooks - Salvando dados do evento

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,35 @@
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb"
+import { stripe } from "../../../services/stripe";
+
+export async function saveSubscription (subscriptionId: string, customerId: string) {
+  // buiscar o usuario no banco fauna db com Id {customerId}
+  // salvar os dados da subscription no fauna db
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(
+        q.Match(
+          q.Index('user_by_stripe_customer_id'),
+          customerId,
+        )
+      )
+    )
+  )
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  }
+
+  await fauna.query(
+    q.Create(
+      q.Collection('subscriptions'),
+      { data: subscriptionData }
+    )
+  )
+}
