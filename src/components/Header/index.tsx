2b3b59609c23037a85f0915b836c8842a3329a50
William Nogueira <will.ribeiro@live.com>
2021-09-18 18:12:01 -0300

    Front-end JAMStack - Trabalhando com CMS - Componente: ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,21 @@
 import { SignInButton } from "../SignInButton"
-import Link from "next/link"
 import styles from "./styles.module.scss"
+import { ActiveLink } from "../ActiveLink"
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
       <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a> 
-          </Link>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a> 
+          </ActiveLink>
           
-          <Link href="/posts">
+          <ActiveLink activeClassName={styles.active} href="/posts">
           <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
