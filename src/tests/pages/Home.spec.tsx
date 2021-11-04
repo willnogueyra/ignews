0b889489fd38e4b527586cc59bb0afc5dec84450
William Nogueira <will.ribeiro@live.com>
2021-11-04 17:26:02 -0300

    testando pagina Home

---
+++ src/tests/pages/Home.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import Home from '../../pages';
+
+jest.mock('next/router')
+
+jest.mock('next-auth/client', () => {
+  return {
+    useSession() {
+      return [null, false]
+    }
+  }
+})
+
+describe('Home Page', () => {
+  it('renders correctly', () => {
+    render(
+      <Home product={{ priceId: 'fake-price-id', amount: 'R$10,00'}} />
+    );
+    
+    expect(screen.getByText('for R$10,00 month')).toBeInTheDocument()
+  })
+})
